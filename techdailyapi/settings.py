de0682f40d15293f7ab557d16a08e2c5f3cdca99
Nazmu Masood <nazmumasood96@gmail.com>
2021-06-29 08:03:55 +0600

    heroku deployment configured

--- techdailyapi/settings.py
+++ techdailyapi/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@d=$k7^89t0dr4$-nd^a^k@s3zy-0f2$dftc$)mim)f$8hc1!m'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-@d=$k7^89t0dr4$-nd^a^k@s3zy-0f2$dftc$)mim)f$8hc1!m')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,8 +85,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'techdailyapi',
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
+        'USER': config('DB_USER', default='postgres'),
+        'PASSWORD': config('DB_PASSWORD', default=''),
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
@@ -134,3 +135,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals(
